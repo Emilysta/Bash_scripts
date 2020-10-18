f89f0a0afa5c3042800582e0b9635b1ef2bdb97c
Emilysta <emiliastarczyk@gmail.com>
2020-10-18 13:22:14 +0200

    Zmiana na fodler

--- powitanie.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-day_number=$(LC_ALL=C date +'%u');
-if [ $day_number -ne 6 -a $day_number -ne 7 ];
-then
-echo " Zapowiada sie pracowity dzien ";
-else
-echo " Milego weekendu"; 
-fi
--- zadanie3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-tar -cvzf $3 $(find . -mtime "$2" -name "$1") > wynik_tar.txt
