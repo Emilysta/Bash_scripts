5fe81bd2700571860f6d082c768b7f60abcfe637
Emilia Starczyk <50081545+Emilysta@users.noreply.github.com>
2020-10-20 10:50:12 +0200

    Komentarz dodatkowy i zmiana nazw
    Zmiana nazw zmiennych środowiskowych oraz dodanie komentarza o znaczeniu ':'

--- LAB1/find_and_tar.sh
+++ LAB1/find_and_tar.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
-
-while  getopts m:n:a: flag
+# dwukropek oznacza argument np. wartość, która jest do wpisania przy danej opcji
+# jeśli wpiszemy ij: oznacza to, że opcja 'i' nie potrzebuje wpisania żadnych dodatkowych wartości.
+# w tym przypadku opcja 'j' wymaga dodkowego argumentu np. liczby albo ciągu znakow, 
+# aby ustawić odpowiednią wartość zmiennej w skrypcie 
+while  getopts m:n:a: flaga
 do
-	case "$flag" in
+	case "$flaga" in
 		m) maska=$OPTARG;;
 		n) dni=$OPTARG;;
-		a) file=$OPTARG;;
+		a) plik=$OPTARG;;
 	esac
 done
 if [ -z "$maska" ]; 
 then
-	if [ -z "$MASKA_PLIKU" ]
+	if [ -z "$MASKA_FINDTAR" ]
 	then
 		echo "Podaj maskę: "
 		read maska;
 	else
-		maska=$MASKA_PLIKU;
+		maska=$MASKA_FINDTAR;
 	fi
 fi
 
 if [ -z "$dni" ] ;
 then
-	if [ -z "$DNI_MODYFIKACJI" ]
+	if [ -z "$DNI_FINDTAR" ]
 	then
 		echo "Podaj dni: "
 		read dni;
 	else
-		dni=$DNI_MODYFIKACJI;
+		dni=$DNI_FINDTAR;
 	fi
 fi
 
 if [ -z "$file" ];
 then
-	if [ -z "$TAR_FILE" ]
+	if [ -z "$NAZWA_FINDTAR" ]
 	then
-		echo "Podaj nazwę tar: "
-		read file;
+		echo "Podaj nazwę archiwum: "
+		read plik;
 	else
-		file=$TAR_FILE;
+		plik=$NAZWA_FINDTAR;
 	fi
 fi
-tar -czf $file $(find . -mtime "-$dni" -name "$maska") 
+tar -czf "$plik" $(find . -mtime "-$dni" -name "$maska") 
