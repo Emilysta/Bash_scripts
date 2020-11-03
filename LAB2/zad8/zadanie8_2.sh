ee8a9f696474e267755efdfd36b22341df5f5d2e
Emilia Starczyk <50081545+Emilysta@users.noreply.github.com>
2020-11-03 15:47:43 +0100

    Update zadanie8_2.sh

--- LAB2/zad8/zadanie8_2.sh
+++ LAB2/zad8/zadanie8_2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+echo $$
+sleep 5
+ulimit -u 400
+nice -n 19 ./zadanie8.sh &
 
-ulimit -u 300
-./zadanie8.sh &
-
-echo "Liczba procesow:" $(ps -u emilia | wc -l)
-echo "Liczba procesow:" $(ps -u emilia | wc -l)
-echo "Liczba procesow:" $(ps -u emilia | wc -l)
+echo Liczba procesow $(ps -u emilia | wc -l)
+sleep 0.5
+echo Liczba procesow $(ps -u emilia | wc -l)
+sleep 0.5
+echo Liczba procesow $(ps -u emilia | wc -l)
 echo "Opanowanie sytuacji - zabijam wszystkie utworzone procesy"
 pkill -9 -f "zadanie8.sh"
-sleep 3
-echo "Koncze prace"
-
 
